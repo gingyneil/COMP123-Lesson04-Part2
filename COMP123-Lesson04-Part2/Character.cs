c4a61149be162b25669334e43fc48f3915782414
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-01-28 09:20:18 -0500

    Added Character Class

---
+++ COMP123-Lesson04-Part2/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson04_Part2
+{
+    // CHARACTER SUPER CLASS
+    class Character
+    {
+
+        // PRIVATE PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++
+        protected int health;
+        protected int smarts;
+        protected string gender;
+        protected string name;
+        
+        // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public Character(string name, int health, int smarts, string gender)
+        {
+            this.name = name;
+            this.health = health;
+            this.smarts = smarts;
+            this.gender = gender; // Gender of the Character
+        }
+
+        // PROTECTED MEHODS +++++++++++++++++++++++++++++++++++++++++++++++++
+        protected void showProfile()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++");
+            Console.WriteLine("Name: {0}", this.name);
+            Console.WriteLine("Health: {0}", this.health);
+            Console.WriteLine("Smarts: {0}", this.smarts);
+            Console.WriteLine("Gender: {0}", this.gender);
+            Console.WriteLine("++++++++++++++++++++++++++");
+        }
+
+    }
+}
